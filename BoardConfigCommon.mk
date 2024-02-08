c1972dc71e2875476452dcdbcf4de66fc4deacf5
Bruno Martins <bgcngm@gmail.com>
2024-02-08 17:05:32 +0000

    sm8475-common: Use the common device compatibility matrix from QCOM
    Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,9 +122,9 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/device_framework_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_SKUS := cape
 DEVICE_MANIFEST_CAPE_FILES += $(COMMON_PATH)/manifest_cape.xml
 
