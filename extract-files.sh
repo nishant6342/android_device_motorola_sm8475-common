f953133ae8f7c7bf530e6560d9fc727b4439ed45
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-06 03:37:29 +0300

    sm8475-common: automatically patch ims
    https://review.lineageos.org/q/topic:%22extract-utils-apktool%22
    
    Change-Id: I704e191adc7aafe6fe062c1bb330fbd1ca05493c
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -107,6 +107,9 @@ function blob_fixup() {
         vendor/lib64/sensors.moto.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v33.so" "${2}"
             ;;
+        system_ext/priv-app/ims/ims.apk)
+            apktool_patch "${2}" "$MY_DIR/ims-patches"
+            ;;
     esac
 }
 
