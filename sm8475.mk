4cc90d7c42f1b564db23d8d9a209856f86a1807d
Aaron Kling <webgeek1234@gmail.com>
2024-08-19 23:48:40 -0500

    sm8475-common: Remove omx libraries
    This arch uses exclusively c2
    
    Change-Id: I306464bc3d9065b557bcf3e024c43282f82e5fe1
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- sm8475.mk
+++ sm8475.mk
@@ -318,15 +318,6 @@ PRODUCT_PACKAGES += \
     MotoActions \
     MotoCommonOverlay
 
-# OMX
-PRODUCT_PACKAGES += \
-    libcodec2_hidl@1.2.vendor \
-    libcodec2_vndk.vendor \
-    libOmxCore \
-    libmm-omxcore \
-    libstagefrighthw \
-    libstagefright_softomx_plugin.vendor
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
