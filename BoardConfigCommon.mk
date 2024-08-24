83fce15f76bdd3aa04cb6d512ac04acd096a47a4
Nishant Kumar <www.rajsonu13@gmail.com>
2024-08-24 11:15:24 +0530

    sm8475-common: Re-add system side qcc blobs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -175,6 +175,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 TARGET_USES_HARDWARE_QCOM_GPS := false
 
 # HIDL
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
