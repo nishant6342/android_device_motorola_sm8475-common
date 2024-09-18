fb9cbd7ef08babfcb74ac0606098d15096d424a9
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-18 18:23:45 +0300

    sm8475-common: use FIX_SONAME
    Change-Id: I41716f84aeceb43b850402ee6ead248dc53ef8b5
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -96,22 +96,6 @@ function blob_fixup() {
         vendor/lib64/libmotext_inf.so)
             ${PATCHELF} --remove-needed "libril.so" "${2}"
             ;;
-        vendor/lib64/libancbase_rt_fusion.so)
-            ${PATCHELF} --set-soname libancbase_rt_fusion.so "${2}"
-            ;;
-        vendor/lib64/libremosaiclib_s5khp1.so)
-            ${PATCHELF} --set-soname libremosaiclib_s5khp1.so "${2}"
-            ;;
-        vendor/lib64/libremosaiclib_s5kjn1.so)
-            ${PATCHELF} --set-soname libremosaiclib_s5kjn1.so "${2}"
-            ;;
-        vendor/lib64/libgrpc++_unsecure_prebuilt.so)
-            ${PATCHELF} --set-soname libgrpc++_unsecure_prebuilt.so "${2}"
-            ;;
-        # rename moto modified primary audio to not conflict with source built
-        vendor/lib64/hw/audio.primary.taro-moto.so)
-            ${PATCHELF} --set-soname audio.primary.taro-moto.so "${2}"
-            ;;
         vendor/lib64/vendor.qti.gnss-service.so)
             ${PATCHELF} --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
             ;;
