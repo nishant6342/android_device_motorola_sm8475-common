9710248cc4dab926a0c73b014d5e409fb39fdff3
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:46 +0300

    sm8475-common: Switch to AIDL USB HAL
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- sm8475.mk
+++ sm8475.mk
@@ -382,7 +382,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti \
+    android.hardware.usb-service.qti \
     init.qcom.usb.rc \
     init.qcom.usb.sh
 
