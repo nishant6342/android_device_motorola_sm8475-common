06eb7ef2163aed33a38eb56612a6c2eccf2c9d23
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-28 03:18:57 +0200

    sm8475-common: move vibrator service to device
    Change-Id: I256ee77bea74775fa084bfb8e5522378a5d1346e
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- sm8475.mk
+++ sm8475.mk
@@ -393,7 +393,8 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # Vibrator
-$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := strict
